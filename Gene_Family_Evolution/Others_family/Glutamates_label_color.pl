8922f80c944d122979670d07395f22171413a3cb
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2022-11-30 12:57:29 +0800

    Create Glutamates_label_color.pl

---
+++ Gene_Family_Evolution/Others_family/Glutamates_label_color.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#node 100379 will also have a filled green rectangle in the middle of the branch, same size as the circle defined above (size is 10)
+#100379,1,10,#00ff00,1,0.5
+
+#leaf label for node 9606 will be displayed in green, bold and twice the regular font size
+#9606 label #00ff00 bold 2
+
+my %clean_clor=(
+        'Ldim'  => '#000000',
+        'Tbif' => '#999999',
+        'Ncel'   => '#e6e6e6',
+        'Smel'      => '#999999',
+        'Tads'   => '#999999',
+        'Lber'    => '#e6e6e6',
+        'Cund'   => '#e6e6e6',
+        'Spul'     => '#999999'
+        );
+
+my %spe_color=(
+        'Ldim'  => '#f44336',
+        'Tbif' => '#744700',
+        'Ncel'   => '#ce7e00',
+        'Smel'      => '#8fce00',
+        'Tads'   => '#2986cc',
+        'Lber'    => '#16537e',
+        'Cund'   => '#6a329f',
+        'Spul'     => '#c90076'
+        );
+
+open FAS, $ARGV[0] or die "can not open $ARGV[0]\n";
+while (<FAS>) {
+        chomp;
+        if (/\>/) {
+#                s/\>//;
+                next if /NonOR/;
+                (my $spe)=$_=~/>(.*?)\_/;
+                if ($clean_clor{$spe} && $spe_color{$spe}) {
+                        my $clean=$clean_clor{$spe};
+                        my $speco=$spe_color{$spe};
+                        s/\>//;
+                        my $info ="$_ label $speco bold 2";
+                        print "$info\n";
+                } else {
+                        s/\>//;
+                        my $info ="$_ label #000000 bold 2";
+                        print "$info\n";
+                }
+        }
+}
